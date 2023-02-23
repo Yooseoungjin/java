e45e7fa06da125e2b56e779d4533a957f827ad4d
Yooseoungjin <yuziyuzi2@gmail.com>
2023-02-23 09:36:09 +0900

    day02

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a=2000000000;
+		int b=2000000000;
+		long sum = (long)a + (long)b;
+		System.out.print(sum);
+	}
+
+}

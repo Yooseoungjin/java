577a3751d40740c4ccd3bf8178b7ab196bf87940
Yooseoungjin <yuziyuzi2@gmail.com>
2023-02-23 16:20:40 +0900

    add

--- day02/src/variable/P82.java
+++ day02/src/variable/P82.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class P82 {
 
 	public static void main(String[] args) {
-		int score = 1;
+		int score = 60;
 				
 		String grade = (score >= 90) ? "A" 
 				: ((score >= 80) ? "B" 
 				: ((score >= 70) ? "C" 
-				: "탈락"));
+				: ((score >= 60) ? "D"
+				: "탈락")));
 			
 		System.out.println(grade);
 

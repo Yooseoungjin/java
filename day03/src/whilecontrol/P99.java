205bfb6f8557fbdb8a12f201d4e80f0184891342
Yooseoungjin <yuziyuzi2@gmail.com>
2023-02-27 09:24:31 +0900

    test를 합니다

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -3,10 +3,12 @@ package whilecontrol;
 public class P99 {
 
 	public static void main(String[] args) {
-		for(int i=2;i<10;i++) {
-			System.out.printf("%d 단 \n",i);
-			for(int j=1;j<10;j++) {
-				System.out.printf("%d * %d = %d \n",i,j,(i*j));
+		
+		int bb = 200;
+		for (int i = 2; i < 10; i++) {
+			System.out.printf("%d 단 \n", i);
+			for (int j = 1; j < 10; j++) {
+				System.out.printf("%d * %d = %d \n", i, j, (i * j));
 			}
 		}
 	}
